eba1f24e94d8f4ccf2ca2a3b6aa06958b300dda6
Róman Demchuk <roman.demchuk@gmail.com>
2023-07-15 14:05:24 +0200

    Create Api connection, add api endpoints with types

---
+++ src/api/apiClient/apiEndpoints.ts
@@ -0,0 +1,10 @@
+import { ApiEndpointsStructure } from "../types";
+
+export const apiEndpoints: ApiEndpointsStructure = {
+  people: "/people",
+  planets: "/planets",
+  films: "/films",
+  species: "/species",
+  vehicles: "/vehicles",
+  starships: "/starships",
+};
