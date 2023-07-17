f7fcaa634b67c743da6c0442d5f06f0ac34b0123
Róman Demchuk <roman.demchuk@gmail.com>
2023-07-17 12:59:14 +0200

    Pass categories as props on the side bar buttons

--- src/components/CategoriesMenu/CategoriesMenu.tsx
+++ src/components/CategoriesMenu/CategoriesMenu.tsx
@@ -3,15 +3,24 @@ import styles from "@/components/CategoriesMenu/CategoriesMenu.module.scss";
 
 interface ButtonProps {
   categories: ApiCategoriesStructure;
+  onClick: (category: string) => void;
 }
 
-const CategoriesMenu = ({ categories }: ButtonProps): React.ReactElement => {
+const CategoriesMenu = ({
+  categories,
+  onClick,
+}: ButtonProps): React.ReactElement => {
   return (
     <aside className={styles["menu"]}>
       <ul className={styles["menu__list"]}>
         {Object.entries(categories).map((category, index) => (
           <li className="menu__item" key={index}>
-            <button className={styles["menu__button"]}>{category[1]}</button>
+            <button
+              className={styles["menu__button"]}
+              onClick={() => onClick(category[1])}
+            >
+              {category[1]}
+            </button>
           </li>
         ))}
       </ul>
